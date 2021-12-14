65550f29ebde5aad5e477d45e67ad15a2669bb6a
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-14 16:14:50 +0100

    some console.log

--- src/utils/db/create-tables.js
+++ src/utils/db/create-tables.js
@@ -2,9 +2,12 @@ import fs from "fs-extra";
 import path from "path";
 import pool from './connect.js'
 
+
 const createTables = async() => {
     try {
-        const fileAsBuffer = await fs.readFile(path.join(process.cwd(), "src/utils/db/tables.sql"))
+        const filePath = path.join(process.cwd(), "./tables.sql");
+        console.log("THIS IS THE FILE PATH", filePath)
+        const fileAsBuffer = await fs.readFile(filePath)
         const fileAsString = fileAsBuffer.toString();
         await pool.query(fileAsString);
         console.log("table created")
