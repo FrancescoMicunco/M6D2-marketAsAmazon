53f7d42032c5fdf0efc86eabc2eb17f99b114ea5
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-14 13:10:18 +0100

    fixed start bug

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import { testConnection } from './utils/db/connect.js'
-import productRouter from './services/products/indexProd.js'
+import express from "express";
+import cors from 'cors';
+import { testConnection } from './utils/db/connect.js';
+import productRouter from './services/products/indexProd.js';
 import reviewsRouter from "./services/reviews/indexRev.js";
 
 const server = express();
@@ -16,7 +16,7 @@ server.use("/reviews", reviewsRouter);
 
 console.table(server)
 server.listen(process.env.PORT || 3001, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running`);
     testConnection()
 
 });
